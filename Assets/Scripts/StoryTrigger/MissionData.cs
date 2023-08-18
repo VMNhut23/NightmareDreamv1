de0c9138cfb147f6611bb9519f602ba6c56add7b
VMNhut23 <vominhnhut151103@gmail.com>
2023-08-18 20:30:13 +0700

    [add mission]

---
+++ Assets/Scripts/StoryTrigger/MissionData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissionData : ScriptableObject
+{
+    public new string name;
+    public bool isDonce;
+}
