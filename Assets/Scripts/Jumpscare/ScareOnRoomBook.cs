a36dc7b05d0a0263824498e94d4c08896ad5a641
VMNhut23 <vominhnhut151103@gmail.com>
2023-08-26 20:08:48 +0700

    [test build and add jumpscare]

--- Assets/Scripts/Jumpscare/ScareOnRoomBook.cs
+++ Assets/Scripts/Jumpscare/ScareOnRoomBook.cs
@@ -8,19 +8,22 @@ public class ScareOnRoomBook : MonoBehaviour
     public Light lightScare;
     public GameObject obj;
     public Animator doorBook;
+    public AudioSource soundRitual;
     private void OnTriggerEnter(Collider other)
     {
         obj.SetActive(true);
         doorBook.Play("DoorBookOff", 0, 0.0f);
         lightScare.enabled = true;
+        soundRitual.Play();
         StartCoroutine(End());
     }
     IEnumerator End()
     {
-        yield return new WaitForSeconds(5f);
+        yield return new WaitForSeconds(10f);
         obj.SetActive(false);
         doorBook.Play("DoorBookOn", 0, 0.0f);
         lightScare.enabled = false;
+        soundRitual.Pause();
         Destroy(gameObject);
     }
 }
