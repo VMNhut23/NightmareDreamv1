13b24ae82ba6008db72a742a8d77d7bd4f5c70b9
VMNhut23 <vominhnhut151103@gmail.com>
2023-09-04 17:19:26 +0700

    [add ui]

---
+++ Assets/Scripts/Constants/GlobalConfig.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class GlobalConfig : ScriptableObject
+{
+    [Header("UI")]
+    public float loadingOverLapTime = 1f;
+}
---
+++ Assets/Scripts/UI/Screen/ScreenHome.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class ScreenHome : BaseScreen
+{
+    private void OnEnable()
+    {
+        if (AudioManager.HasInstance)
+        {
+            //AudioManager.Instance.PlayBGM(AUDIO.BGM_BGM_3);
+        }
+    }
+
+    public override void Show(object data)
+    {
+        base.Show(data);
+    }
+
+    public override void Hide()
+    {
+        base.Hide();
+    }
+
+    public void OnClickPopupSetting()
+    {
+        if (UIManager.HasInstance)
+        {
+            UIManager.Instance.ShowPopup<PopupSetting>();
+        }
+    }
+
+    public void StartGame()
+    {
+        if (UIManager.HasInstance)
+        {
+            UIManager.Instance.ShowNotify<NotifyLoadingGame>();
+        }
+        this.Hide();
+    }
+    public void EndGame()
+    {
+        Application.Quit();
+    }
+}
